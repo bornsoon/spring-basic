203f3619bc73435819403895166c694b540d4978
hyolim922@hotmail.com <hyolim922@hotmail.com>
2025-07-25 00:42:04 +0900

    fix: 스프링 빈 적용

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration  // 애플리케이션의 구성(설정) 정보
 public class AppConfig {
 
     // 생성자 주입
+    @Bean // 스프링 컨테이너에 등록 (key: 메소드명, value: 반환값)
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());   // Ctrl + Alt + M > Extract Method
     }
 
+    @Bean
     private static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy() );
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -5,14 +5,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
         // 구현 객체의 선택은 appConfig가 담당
-        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
+
+        // Bean 객체 관리하는 개체
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// (bean 이름, 반환타입)
+
         // Command + Option + v / Ctrl + Alt + v
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
