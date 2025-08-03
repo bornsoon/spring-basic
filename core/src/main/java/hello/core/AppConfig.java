0e135a0179db83d21c3cdf169097aa4247ce5748
hyolim922@hotmail.com <hyolim922@hotmail.com>
2025-08-03 15:15:39 +0900

    feat: @Config + @Bean -> SingletonTest

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -14,20 +14,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration  // 애플리케이션의 구성(설정) 정보
 public class AppConfig {
 
+    // @Bean MemberService 생성 -> new MemoryMemberRepository() 호출
+    // @Bean orderService 생성 -> new MemoryMemberRepository() 호출
+    // 2개의 MemoryMemberRepository가 생성되면서 싱글톤 깨짐?
+
     // 생성자 주입
     @Bean // 스프링 컨테이너에 등록 (key: 메소드명, value: 반환값)
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());   // Ctrl + Alt + M > Extract Method
     }
 
     @Bean
-    private static MemoryMemberRepository memberRepository() {
+    public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
+    //`@Configuration + @Bean`을 통해 빈으로 등록할 때,
+    // static 메서드로 할 경우 "싱글톤" 보장해주지 못함!!!!!
+    // private static MemoryMemberRepository memberRepository() {return new MemoryMemberRepository();}
 
     @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), discountPolicy() );
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
@@ -35,4 +45,10 @@ public class AppConfig {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
+
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository -> X
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository-> X
+    // call AppConfig.memberRepository
 }
