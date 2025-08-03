37e628c58d886c2c3f2661e3e318b4652e0fab8e
hyolim922@hotmail.com <hyolim922@hotmail.com>
2025-08-03 17:07:41 +0900

    feat: @Component + @Autowired 로 자동으로 빈 등록, 의존 관계 주입

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -9,7 +9,7 @@ public class MemberServiceImpl implements MemberService {
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
 
-    // 의존 관계 자동 주입
+    // 의존 관계 자동 주입 - 기본은 타입이 일치하는 빈 주입
     @Autowired  // ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
