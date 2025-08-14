6f24386c8be00b0ac754de6285c41586feff7b3a
hyolim922@hotmail.com <hyolim922@hotmail.com>
2025-08-14 15:36:50 +0900

    fix: @StringBootTest 주석 추가

--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest // 스프링 컨테이너를 띄어서 통합해서 테스트
 class CoreApplicationTests {
 
 	@Test
