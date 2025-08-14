46e7bc516ffb18802c555858aac35aad2aa2533c
hyolim922@hotmail.com <hyolim922@hotmail.com>
2025-08-14 16:39:39 +0900

    feat: 생성자 주입 권장 이유

---
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+        /* 수정자 주입 이용하면 아래 코드는 컴파일 오류 X
+           생성자 주입 이요하면 필드 값이 필수이기 때문에 컴파일 오류 발생
+        OrderServiceImpl orderService = new OrderServiceImpl(); */
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
